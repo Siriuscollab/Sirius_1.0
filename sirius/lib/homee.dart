3d27f3649aa3beb92d5e4a6d96a7c8601ce46be4
ravikiran724 <45527734+ravikiran724@users.noreply.github.com>
2020-09-03 12:41:07 +0530

    Add files via upload

--- sirius/lib/homee.dart
+++ sirius/lib/homee.dart
@@ -104,6 +104,9 @@ class _FireState extends State<Fire> {
                       return GestureDetector(
                         onTap: (){
                           print('ok');
+                          Navigator.push(context, MaterialPageRoute(builder: (context){
+                            return detailview(list:lists[index]);
+                          }));
                         },
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
@@ -333,6 +336,74 @@ class _HomState extends State<Hom> {
               ),
             ),
       ),
+    );
+  }
+}
+class detailview extends StatefulWidget {
+  Map<dynamic,dynamic> list;
+  detailview({this.list});
+  @override
+  _detailviewState createState() => _detailviewState();
+}
+
+class _detailviewState extends State<detailview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text(
+          'Project Details'
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+        // mainAxisAlignment: MainAxisAlignment.center ,
+        children: <Widget>[
+            Text('Project Title',
+            style: TextStyle(
+              fontSize: 21.0,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic
+            ),
+            ),
+            Text(widget.list['title'],
+              style: TextStyle(
+                  fontSize: 21.0,
+                  // fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic
+              ),),
+            Text('Project Description',
+              style: TextStyle(
+                  fontSize: 21.0,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic
+              ),),
+            Text(widget.list['description'],
+              style: TextStyle(
+                  fontSize: 21.0,
+                  // fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic
+              ),),
+            Text('Group Size',
+              style: TextStyle(
+                  fontSize: 21.0,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic
+              ),),
+            Text(widget.list['groupsize'],
+              style: TextStyle(
+                  fontSize: 21.0,
+                  // fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic
+              ),),
+
+        ],
+    ),
+          ),
     );
   }
 }
